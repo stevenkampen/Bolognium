44af8d582b699d8e62cfe0dafd5527bc5e72be4d
stevensherrie <steve@wasteofpaper.com>
2011-11-21 23:40:41 -0500

    Initial Upload

--- config/base.py
+++ config/base.py
@@ -7,8 +7,8 @@
 debug = True
 debug_value_cache_time = 60
 
-aes_key = u'TisG!7nrPMb8S3&QnyHSJT@?LEr7hAmv'
-aes_iv = u'BsKzU5QkqC5d4Gvn'
+aes_key = u'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+aes_iv = u'xxxxxxxxxxxxxxxx'
 
 domain = u'http://www.wasteofpaper.com'
 secure_domain = u'https://www.wasteofpaper.com'
