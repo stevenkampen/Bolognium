05054db4bbda169e47f136df6002d7932bcc6770
Steven Sherrie <steve@wasteofpaper.com>
2011-12-15 13:47:15 -0500

    Cleaning up.

--- config/base.py
+++ config/base.py
@@ -7,9 +7,6 @@
 debug = True
 debug_value_cache_time = 60
 
-aes_key = u'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-aes_iv = u'xxxxxxxxxxxxxxxx'
-
 site_name = u'bolognium-source.appspot.com'
 domain = u'http://bolognium-source.appspot.com'
 secure_domain = u'https://bolognium-source.appspot.com'
